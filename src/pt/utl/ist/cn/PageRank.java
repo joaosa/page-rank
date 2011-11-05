d5d3db0eb4f6617941e4a21166a7986892e688dd
joaosa <jjadonotenter@gmail.com>
2011-11-05 16:13:37 +0000

    source folder

---
+++ src/pt/utl/ist/cn/PageRank.java
@@ -0,0 +1,67 @@
+package pt.utl.ist.cn;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class PageRank {
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+
+		private Text urls = new Text();
+
+		// private final static IntWritable initPR = new IntWritable(1);
+
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			StringTokenizer tokenizer = new StringTokenizer(value.toString());
+			while (tokenizer.hasMoreTokens()) {
+				urls.set(tokenizer.nextToken());
+				context.write(new Text(key.toString()), urls);
+			}
+		}
+
+	}
+
+	public static class Reduce extends Reducer<Text, Text, Text, Text> {
+
+		public void reduce(Text key, Text values, Context context)
+				throws IOException, InterruptedException {
+			context.write(key, values);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+
+		Job job = new Job(conf, "PageRank");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		// Note that these are the default.
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		job.setMapperClass(Map.class);
+		job.setCombinerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
+
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
---
+++ src/pt/utl/ist/cn/WordCount.java
@@ -0,0 +1,73 @@
+package pt.utl.ist.cn;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class WordCount {
+
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+		private Text word = new Text();
+		private final static IntWritable one = new IntWritable(1);
+
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			StringTokenizer tokenizer = new StringTokenizer(value.toString());
+			while (tokenizer.hasMoreTokens()) {
+				word.set(tokenizer.nextToken());
+				context.write(word, one);
+			}
+		}
+
+	}
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+
+		Job job = new Job(conf, "Hadoop 0.21 WordCount");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		// Note that these are the default.
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		job.setMapperClass(Map.class);
+		job.setCombinerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
+
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+}
